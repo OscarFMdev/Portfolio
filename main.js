a6a1abdfad5ed834fd28fc19eb7c0aa1aeba0c26
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-06 01:51:07 +0430

    Added toggle behavior to the mobile nav

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-const openMenu = document.querySelector('.hamburger');
-const menuOptions = document.querySelector('.menu-options');
-openMenu.addEventListener('click', () => {
+const openMenu = document.querySelector(".hamburger");
+const menuOptions = document.querySelector(".menu-options");
+openMenu.addEventListener("click", () => {
+  menuOptions.classList.toggle("open");
+
+  if (menuOptions.classList.contains("open")) {
+    menuOptions.style.height = "100vh";
+    openMenu.classList.remove("fa-bars");
+    openMenu.classList.add("fa-close", "text-white");
+  } else {
+    menuOptions.style.height = "0";
+    openMenu.classList.remove("fa-close", "text-white");
+    openMenu.classList.add("fa-bars");
+  }
+});
+
+window.addEventListener('resize', () => {
+    if(window.innerWidth < 768) {
+        menuOptions.classList.remove("open");
+    }
 })
