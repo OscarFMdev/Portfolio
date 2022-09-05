23c2e4ef3e6668a61803591fb0a9edeb210b038f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-06 02:31:08 +0430

    Update JS file quotation problem

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-const openMenu = document.querySelector(".hamburger");
-const menuOptions = document.querySelector(".menu-options");
-openMenu.addEventListener("click", () => {
-  menuOptions.classList.toggle("open");
+const openMenu = document.querySelector('.hamburger');
+const menuOptions = document.querySelector('.menu-options');
+openMenu.addEventListener('click', () => {
+  menuOptions.classList.toggle('open');
 
-  if (menuOptions.classList.contains("open")) {
-    menuOptions.style.height = "100vh";
-    openMenu.classList.remove("fa-bars");
-    openMenu.classList.add("fa-close", "text-white");
+  if (menuOptions.classList.contains('open')) {
+    menuOptions.style.height = '100vh';
+    openMenu.classList.remove('fa-bars');
+    openMenu.classList.add('fa-close', 'text-white');
   } else {
-    menuOptions.style.height = "0";
-    openMenu.classList.remove("fa-close", "text-white");
-    openMenu.classList.add("fa-bars");
+    menuOptions.style.height = '0';
+    openMenu.classList.remove('fa-close', 'text-white');
+    openMenu.classList.add('fa-bars');
   }
 });
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   if (window.innerWidth < 768) {
-    menuOptions.classList.remove("open");
-    openMenu.classList.remove("fa-close", "text-white");
+    menuOptions.classList.remove('open');
+    openMenu.classList.remove('fa-close', 'text-white');
   } else {
-    menuOptions.style.height = "auto";
+    menuOptions.style.height = 'auto';
   }
 });
 
-menuOptions.addEventListener("click", () => {
-  openMenu.classList.remove("fa-close", "text-white");
-  menuOptions.classList.remove("open");
-  openMenu.classList.add("fa-bars");
+menuOptions.addEventListener('click', () => {
+  openMenu.classList.remove('fa-close', 'text-white');
+  menuOptions.classList.remove('open');
+  openMenu.classList.add('fa-bars');
 });
