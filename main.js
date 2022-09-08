4704327369a523277ddb0f497e0dc86f9a271b94
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-09-08 20:27:49 +0430

    Added regular exprision

--- main.js
+++ main.js
@@ -272,6 +272,19 @@ for (let index = 0; index < modalButton.length; index += 1) {
 
 /* FORM validation */
 
-const errorMessage = document.querySelector('error');
-const emailInput = document.querySelector('input[type=email]');
+const errorMessage = document.querySelector('.error');
+const emailInput = document.querySelector('input[type="email"]');
 
+function formSubmit() {
+  const regex = /^[a-z0-9]+@[a-z0-9-]+\.[a-z0-9-.]+$/;
+  // const email = emailInput.value;
+  // if(!regex.test(email)){
+  //   errorMessage.style.display = 'block'; 
+  //   errorMessage.innerHTML = "The email should only content lowercase letters";
+  //   return false;
+  // }
+  // return true;
+}
+
+// const form = document.querySelector('.form-container');
+// form.addEventListener('submit', formSubmit);
